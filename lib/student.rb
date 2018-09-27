b090a951ac57be2d814590fa116c36b56d27e70b
Nicholas Smith <nico.ehs05@gmail.com>
2018-09-27 14:16:38 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -82,6 +82,6 @@ class Student
   end
   
   def self.all_students_in_grade_X(x)
-    db.execute("SELECT * FROM students WHERE grade=?", id_num).flatten
+    db.execute("SELECT * FROM students WHERE grade=?", x).flatten
   end
 end
